a1fae7a5178a9027529bfc94b8c5ffabecc3ec30
Variona <5630669+Variona@users.noreply.github.com>
2022-12-02 14:15:18 -0500

    Day 1

---
+++ Day 1/Day1.py
@@ -0,0 +1,11 @@
+f = open("calories.txt", "r")
+
+elves = []
+curCal = 0
+for i in f.readlines():
+	if i == "\n":
+		elves.append(curCal)
+		curCal = 0
+		continue
+	curCal += (int(i.strip("\n")))
+print(max(elves))
