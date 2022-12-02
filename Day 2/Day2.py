daa8b8e50fb6f2c89a8e1c53e5e1b5955e5b86f5
Variona <5630669+Variona@users.noreply.github.com>
2022-12-02 15:20:28 -0500

    Update Day2.py

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -1,17 +1,44 @@
-f = open("calories.txt", "r")
 
-elves = []
-curCal = 0
+wins = {"A": "Z", "B": "X", "C": "Y"}
+draws = {"A": "X", "B": "Y", "C": "Z"}
+pointTable = {"X": 1, "Y": 2, "Z": 3}
+def draw(opponent, you):
+	if draws[opponent] == you:
+		return True
+	return False
+	
+
+def win(opponent, you):
+	if you == wins[opponent]:
+		return 0
+		
+	return 6
+
+def calculate(input): #could prob merge this function with win()
+	opponent = input[0]
+	you = input[1]
+	points = pointTable[you]
+	
+	if not draw(opponent, you):
+		points += win(opponent, you)
+	else:
+		points += 3
+	return points
+		
+#creating a list of lists to iterate over		
+f = open("guide.txt", "r")
+tempList = []
+guideList = []
 for i in f.readlines():
-	if i == "\n":
-		elves.append(curCal)
-		curCal = 0
-		continue
-	curCal += (int(i.strip("\n")))
+	for i2 in i.strip():
+		tempList.append(i2.strip())
+	tempList.pop(1)
+	guideList.append(tempList)
+	tempList = []
 f.close()
 
-curCal = 0
-for i in range(3):
-	maxIndex = elves.index(max(elves))
-	curCal += elves.pop(maxIndex)
-print(curCal)
+#summing every match
+totalPoints = 0
+for i in guideList:
+	totalPoints += calculate(i)
+print(totalPoints)
