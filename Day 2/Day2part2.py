16dc4e8e4a7c06a4a93861e6ba4793d73ac813ad
Variona <5630669+Variona@users.noreply.github.com>
2022-12-02 15:55:00 -0500

    Add files via upload

---
+++ Day 2/Day2part2.py
@@ -0,0 +1,43 @@
+wins = {"A": "C", "B": "A", "C": "B"}
+pointTable = {"A": 1, "B": 2, "C": 3}
+def win(opponent, choice):
+	for k, v in wins.items():
+		if v == opponent:
+			return k
+	
+def calculate(input):
+	opponent = input[0]
+	choice = input[1]
+	if choice == "X":
+		decision = wins[opponent]
+		points = 0
+	elif choice == "Y":
+		decision = opponent
+		points = 3
+	else:
+		decision = win(opponent, choice)
+		points = 6
+	#print(decision, end =" ")
+	points += pointTable[decision]
+	return points 
+	
+		
+#creating a list of lists to iterate over		
+f = open("guide.txt", "r")
+tempList = []
+guideList = []
+for i in f.readlines():
+	for i2 in i.strip():
+		tempList.append(i2.strip())
+	tempList.pop(1)
+	guideList.append(tempList)
+	tempList = []
+f.close()
+
+#summing every match
+totalPoints = 0
+tempList = (["A", "X"], ["A", "Y"], ["A", "Z"])
+for i in guideList:
+	totalPoints += calculate(i)
+	
+print(totalPoints)
