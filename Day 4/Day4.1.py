9d059c30d174f76c294401cc85e16af5f32039f9
Variona <5630669+Variona@users.noreply.github.com>
2022-12-04 19:58:13 -0500

    Day 4

---
+++ Day 4/Day4.1.py
@@ -0,0 +1,32 @@
+def intifier(input):
+	inputList = []
+	for i in input.split("-"):
+		inputList.append(int(i))
+	return inputList
+
+def rangeAnalyzer(input1, input2):
+	if (input1[0] <= input2[0]) and (input1[1] >= input2[1]):
+		return True
+	elif (input2[0] <= input1[0]) and (input2[1] >= input1[1]):
+		return True
+
+def pairChecker(input):
+	pair1, pair2 = intifier(input[0]), intifier(input[1])
+	if rangeAnalyzer(pair1, pair2):
+		return True
+
+pairs = []
+f = open("assignments.txt", "r")
+for i in f.readlines():
+	pairs.append(i.strip())
+f.close()
+
+partedPairs = []
+for i in pairs:
+	partedPairs.append(i.split(","))
+	
+counter = 0
+for i in partedPairs:
+	if pairChecker(i):
+		counter += 1
+print(counter)
